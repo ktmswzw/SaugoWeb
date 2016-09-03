defb220728f7061ed39b2d0a6f4d199c3bf48b4a
MOXZ <ktmswzw@gmail.com>
2016-09-03 13:51:25 +0800

    update

--- src/main/webapp/styles/js/module/security/user/agentList.js
+++ src/main/webapp/styles/js/module/security/user/agentList.js
@@ -3,7 +3,7 @@ requirejs(['jquery', 'bootstrap','table', 'tablezn', 'select', 'selectCN', 'tExp
         var $OK = $.scojs_message.TYPE_OK;
         var $ERROR = $.scojs_message.TYPE_ERROR;
 
-        initSelect("status", WEB_GLOBAL_CTX+"/business/dictionary/getDropDown", {dicName: 'USER-STATUS'}, "check", "dicKey", "dicValue",true);
+        initSelect("status", WEB_GLOBAL_CTX+"/business/dictionary/getDropDown", {dicName: 'USER-STATUS'}, new Array("check"), "dicKey", "dicValue",true);
 
         //列表
         var $table = $('#tableB').bootstrapTable({
